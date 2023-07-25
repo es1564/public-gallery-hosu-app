2cd2863917e17f617e330ab13cf1b2ccf0099b6a
es1564 <111486640+es1564@users.noreply.github.com>
2023-07-25 13:18:12 +0900

    초기화면!!

--- app/screens/RootStack.js
+++ app/screens/RootStack.js
@@ -1,10 +1,19 @@
 import React from "react";
+import SignInScreen from "./SignInScreen";
 const { createNativeStackNavigator } = require("@react-navigation/native-stack");
 
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
-    return <Stack.Navigator></Stack.Navigator>
+    return (
+        <Stack.Navigator>
+            <Stack.Screen 
+                name="SignIn"
+                component={SignInScreen}
+                options={{headerShown: false}}
+            />
+        </Stack.Navigator>
+    )
 }
 
 export default RootStack;
