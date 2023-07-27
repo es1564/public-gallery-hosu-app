2bc1601422afcd72dfe57dfb70e20658438060e6
es1564 <111486640+es1564@users.noreply.github.com>
2023-07-27 19:24:29 +0900

    포스트 조회

--- app/screens/FeedScreen.js
+++ app/screens/FeedScreen.js
@@ -1,23 +1,79 @@
-import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Image, RefreshControl, StyleSheet, Text, View } from "react-native";
+import { PAGE_SIZE, getOlderPosts, getPosts, getNewerPosts } from "../lib/posts";
+import PostCard from "../components/PostCard";
 
 function FeedScreen() {
+    const [posts, setPosts] = useState(null);
+    const [noMorePost, setNoMorePost] = useState(false);
+    const [refreshing, setRefreshing] = useState(false);
+
+    useEffect(() => {
+        getPosts().then(setPosts);
+    }, [])
+
+    const onLoadMore = async () => {
+        if (noMorePost || !posts || posts.length < PAGE_SIZE) {
+            return;
+        }
+        const lastPost = posts[posts.length - 1];
+        const olderPosts = await getOlderPosts(lastPost.id);
+        if (olderPosts.length < PAGE_SIZE) {
+            setNoMorePost(true);
+        }
+        setPosts(posts.concat(olderPosts));
+    }
+
+    const onRefresh = async () => {
+        if (!posts || posts.length === 0 || refreshing) {
+            return;
+        }
+        const firstPost = posts[0];
+        setRefreshing(true);
+        const newerPosts = await getNewerPosts(firstPost.id);
+        setRefreshing(false);
+        if (newerPosts.length === 0) {
+            return;
+        }
+        setPosts(newerPosts.concat(posts));
+    }
+
     return (
-        <View style={styles.block}>
-            <Text style={styles.text}>Hello</Text>
-        </View>
+        <FlatList
+            data={posts}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            contentContainerStyle={styles.container}
+            onEndReached={onLoadMore}
+            onEndReachedThreshold={0.75}
+            ListFooterComponent={
+                !noMorePost && (
+                    <ActivityIndicator style={styles.spinner} size={32} color="#6200ee" />
+                )
+            }
+            refreshControl={
+                <RefreshControl onRefresh={onRefresh} refreshing={refreshing} />
+            }
+        />
     )
 }
 
+const renderItem = ({item}) => (
+    <PostCard 
+        createdAt={item.createdAt}
+        description={item.description}
+        id={item.id}
+        user={item.user}
+        photoURL={item.photoURL}
+    />
+)
 
 const styles = StyleSheet.create({
-    block: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+    container: {
+        paddingBottom: 48,
     },
-    text: {
-        fontSize: 24,
+    spinner: {
+        height: 64,
     },
 });
 
